88ad7387ab1e4bfa94497da1ec84af39690031db
mahammedzkhan <mkhan@prophets.be>
2015-04-30 16:47:21 +0200

    added importing ability

--- tasks/db_dump.js
+++ tasks/db_dump.js
@@ -19,6 +19,7 @@ var shell = require('shelljs'),
  */
 var commandTemplates = {
   mysqldump: "mysqldump --hex-blob -h <%= host %> -P <%= port %> -u<%= user %> <%= pass %> <%= database %> <%=extra_args%>",
+  mysqlimport: "mysql -h <%= host %> -P <%= port %> -u<%= user %> <%= pass %> <%= database %> < <%= dumpfile %>",
   ssh: "ssh <%= host %>"
 };
 
@@ -46,6 +47,28 @@ module.exports = function (grunt) {
       grunt.log.success("Database dump succesfully exported");
     } else {
       grunt.log.fail("Database dump failed!");
+      return false;
+    }
+  });
+
+  grunt.registerMultiTask('db_import', 'Import database', function() {
+    // Get tasks options + set default port
+    var options = this.options({
+      pass: "",
+      port: 3306,
+      backup_to: "db/backups/<%= grunt.template.today('yyyy-mm-dd') %> - <%= target %>.sql"
+    });
+    
+    var paths = generate_backup_paths(this.target, options);
+    
+    grunt.log.subhead("Importing database '" + options.title + "' from '" + paths.file + "'");
+    if(db_import(options, paths))
+    {
+      grunt.log.success("Database dump succesfully imported");
+    }
+    else
+    {
+      grunt.log.fail("Database import failed!");
       return false;
     }
   });
@@ -116,4 +139,68 @@ module.exports = function (grunt) {
 
     return true;
   }
+
+  /**
+     * Import a MYSQL dumpfile to a database
+     */
+    function db_import(options, paths) {
+        var cmd;
+
+
+        // 2) Compile MYSQL cmd via Lo-Dash template string
+      //
+      // "Process" the password flag directly in the data hash to avoid a "-p" that would trigger a password prompt
+      // in the shell
+        var tpl_mysqlimport = grunt.template.process(commandTemplates.mysqlimport, {
+            data: {
+              user: options.user,
+              pass: options.pass != "" ? '-p' + options.pass : '',  
+              database: options.database,
+              host: options.host,
+              port: options.port,
+              dumpfile: options.backup_to
+            }
+        });
+
+         var file = grunt.file.read(options.backup_to);
+
+
+        grunt.file.write(options.backup_to, file);
+
+
+
+        // 3) Test whether we should connect via SSH first
+        if (typeof options.ssh_host === "undefined") 
+        { 
+            // it's a local/direct connection            
+            cmd = tpl_mysqlimport;
+
+        } 
+        else 
+        { 
+            // it's a remote connection
+            var tpl_ssh = grunt.template.process(commandTemplates.ssh, {
+                data: {
+                    host: options.ssh_host
+                }
+            });
+            
+            cmd = tpl_ssh + " \\ " + tpl_mysqlimport;
+        }
+
+        // Capture output...
+        var ret = shell.exec(cmd, {silent: true});
+     
+      if(ret.code != 0) {
+        grunt.log.error(ret.output)
+        return false;
+      }
+      else {
+        var file = grunt.file.read(options.backup_to);
+
+        grunt.file.write(options.backup_to, file);
+          
+        return true;
+      }
+    }
 };
